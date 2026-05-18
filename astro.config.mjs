080ea824d3f460448ab0e41c455c4ce33de05d0a
rausgucken <rausgucken@gmail.com>
2026-05-18 21:50:14 +0200

    seo: trailingSlash always + fix internal hrefs in Base.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://www.rausgucken.de",
-  trailingSlash: "never",
+  trailingSlash: "always",
   output: "static",
   build: {
     assets: "_assets",
